6524c8bf30c2c4c0c08b4ac54285ea151667646a
adarsh Singh <adarsh Singh>
2016-10-20 12:01:23 +0530

    Corrected

--- HackerEarth/FindProduct.java
+++ HackerEarth/FindProduct.java
@@ -7,9 +7,9 @@ public class FindProduct {
         Scanner sc = new Scanner(System.in);
         int length = sc.nextInt();
         sc.nextLine();
-        int prod = 1;
+        long prod = 1;
         for(int i = 0; i < length; i++)
-            prod = prod * sc.nextInt();
+            prod = (prod * sc.nextInt()) % (long)(Math.pow(10, 9) + 7);
         System.out.println(prod);
         sc.close();
     }
